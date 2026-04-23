e717e3bab69ebee8af9ba38aa19d33890087dc91
Waffle <csshin9928@gmail.com>
2026-04-23 12:31:08 +0900

    feat: add base Component class with positioning, sizing, and lifecycle methods

---
+++ include/components/component.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "types/math.hpp"
+#include <raylib.h>
+
+class Component {
+public:
+  bool enabled = true;
+  bool visible = true;
+
+  UDim2 position;
+  UDim2 size;
+
+  Component() = default;
+  virtual ~Component() = default;
+
+  virtual void Draw() const = 0;
+  virtual void Update() = 0;
+
+  Vector2 GetAbsolutePosition() const;
+  Vector2 GetAbsoluteSize() const;
+};
